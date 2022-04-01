afe91052567e34b6369316636725fc67d30814f0
kamyshenJava <kamyshen@tutanota.com>
2022-04-02 02:12:25 +0300

    1. Quartz [#175122]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,18 +2,37 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+
+    private int getInterval() {
+        int rsl = 0;
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties properties = new Properties();
+            properties.load(in);
+            rsl = Integer.parseInt(properties.getProperty("rabbit.interval"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
     public static void main(String[] args) {
+        AlertRabbit alertRabbit = new AlertRabbit();
+        int interval = alertRabbit.getInterval();
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(interval)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
